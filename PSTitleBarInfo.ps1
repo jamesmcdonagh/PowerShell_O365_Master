0216fe4053c3daa9184b742c2c9b4782b0779842
Maya <jameswmcdonagh@gmail.com>
2018-11-14 11:47:21 -0700

    Update
     Added script to slap date/time in your PS Title Bar

---
+++ PSTitleBarInfo.ps1
@@ -0,0 +1,20 @@
+﻿$code = 
+{
+    # submit the host process RawUI interface and the execution context
+    param($RawUi)
+
+    do
+    {
+        # compose the time and date display
+        $time = Get-Date -Format 'HH:mm:ss dddd MMMM d'
+        # compose the title bar text
+        $title = "Current Time: $time" 
+        # output the information to the title bar of the host process
+        $RawUI.WindowTitle = $title
+        # wait a half second
+        Start-Sleep -Milliseconds 500
+    } while ($true)
+}
+$ps = [PowerShell]::Create()
+$null = $ps.AddScript($code).AddArgument($host.UI.RawUI)
+$handle = $ps.BeginInvoke() 
